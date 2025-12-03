e84dfcbbafbe077673527a5464cb10836d6ebcf5
hirrolot <hirrolot@gmail.com>
2025-12-03 01:10:50 -0500

    Refactor `lib/mechanics/process_graph.ml`

--- lib/mechanics/process_graph.ml
+++ lib/mechanics/process_graph.ml
@@ -18,9 +18,8 @@ type metadata =
 
 let compute_metadata (graph : t) : metadata =
     let f_gensym = Gensym.create ~prefix:"f" () in
-    let metadata =
-        ref { symbol_table = Symbol_map.empty; fresh_to_source_vars = Symbol_map.empty }
-    in
+    let symbol_table = ref Symbol_map.empty in
+    let fresh_to_source_vars = ref Symbol_map.empty in
     let rec go = function
       | Step step -> go_step step
       | Bind (bindings, binder) -> go_binder ~bindings binder
@@ -30,38 +29,30 @@ let compute_metadata (graph : t) : metadata =
       | Driver.Unfold graph -> go graph
       | Driver.Analyze (_x, graph, variants) ->
         go graph;
-        List.iter
-          (fun (Driver.{ c = _; fresh_vars; source_vars }, (binding, graph)) ->
-             List.iter2
+        variants
+        |> List.iter (fun (Driver.{ c = _; fresh_vars; source_vars }, (binding, graph)) ->
+          fresh_vars
+          |> List.iter2
                (fun x y ->
-                  metadata
-                  := { !metadata with
-                       fresh_to_source_vars =
-                         Symbol_map.add x y !metadata.fresh_to_source_vars
-                     })
-               fresh_vars
+                  fresh_to_source_vars := Symbol_map.add x y !fresh_to_source_vars)
                source_vars;
-             match binding with
-             | Some binding -> go_extract (binding, graph)
-             | None -> go graph)
-          variants
+          match binding with
+          | Some binding -> go_extract (binding, graph)
+          | None -> go graph)
       | Driver.Extract (binding, graph) -> go_extract (binding, graph)
     and go_extract ((x, call), graph) = go_binder ~bindings:[ x, call ] (Split graph)
     and go_binder ~bindings binder =
         List.iter (fun (_x, graph) -> go graph) bindings;
         match binder with
-        | Fold node_id when not (Symbol_map.mem node_id !metadata.symbol_table) ->
+        | Fold node_id when not (Symbol_map.mem node_id !symbol_table) ->
           let f, params =
               Gensym.emit f_gensym, List.map (fun (x, _graph) -> x) bindings
           in
-          metadata
-          := { !metadata with
-               symbol_table = Symbol_map.add node_id (f, params) !metadata.symbol_table
-             }
+          symbol_table := Symbol_map.add node_id (f, params) !symbol_table
         (* We have already generated a symbol for this function. *)
         | Fold _ -> ()
         | Generalize graph | Split graph -> go graph
     in
     go graph;
-    !metadata
+    { symbol_table = !symbol_table; fresh_to_source_vars = !fresh_to_source_vars }
 ;;
